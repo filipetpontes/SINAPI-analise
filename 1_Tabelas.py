a6872ce3a82d7132f368bd66cd44f46eca276f37
Filipe Pontes <106703491+filipetpontes@users.noreply.github.com>
2024-12-10 00:48:18 -0300

    Add files via upload

--- 1_Tabelas.py
+++ 1_Tabelas.py
@@ -167,7 +167,7 @@ if st.session_state["df"] is not None:
     if grid_response['selected_rows'] is not None:
         codigo = grid_response['selected_rows']['Código'].iloc[0]
         st.write(codigo)
-        st.session_state["codigo"] = codigo
+        # st.session_state["codigo"] = codigo
 
     if st.button("Detalhar"):
         if grid_response['selected_rows'] is not None:
